60cec0ae2a7814c6c206135de087ba5275dc39a6
siwonhansamu <siwon.hansamu@gmail.com>
2017-12-08 06:52:10 +0700

    Tako Apps

--- app/src/main/java/hackjak/pantidev/tako/Map.java
+++ app/src/main/java/hackjak/pantidev/tako/Map.java
@@ -17,6 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -65,7 +66,7 @@ public class Map extends FragmentActivity implements LocationListener,
 
     TextView tvNama, tvAlamat, tvLuas, tvResmi, tvEmail, tvRepLingkungan, tvRepKejahatan,tvLaktasi;
     Button btnReport;
-    Switch swTraffic;
+    SwitchCompat swTraffic;
 
     GoogleApiClient mGoogleApiClient;
     LocationRequest mLocationRequest;
